7080351e337e4981bd963af462e8b22046e4aee1
Joerick Amadora <124227727+joricxxx@users.noreply.github.com>
2025-03-14 20:56:05 +0800

    Update index.vue
    - Able to Login ( Can verify data if the account is present in the firestore )

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class="min-h-screen flex items-center justify-center">
-        <form class="p-8 rounded shadow-md space-y-4 w-full max-w-md">
+        <form @submit.prevent="handleLogin" class="p-8 rounded shadow-md space-y-4 w-full max-w-md">
             <h1 class="text-2xl font-bold text-center">Log In</h1>
-            <UiVeeInput label="Username" placeholder="Username" />
-            <UiVeeInput label="Password" type="password" />
+            <UiVeeInput v-model="form.username" label="Username" placeholder="Username" required/>
+            <UiVeeInput v-model="form.password" label="Password" type="password" required/>
 
             <UiButton type="submit" class="w-full flex justify-center">Log In</UiButton>
             <p class="text-center mt-4">
@@ -16,6 +16,37 @@
 </template>
 
 <script lang="ts" setup>
+import { initializeApp } from 'firebase/app';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const firebaseConfig = useRuntimeConfig().public;
+const firebaseApp = initializeApp(firebaseConfig)
+const db = getFirestore(firebaseApp)
+const router = useRouter()
+
+const form = ref({
+  username: '',
+  password: ''
+})
+const handleLogin = async () => {
+  if(form.value.username === '' || form.value.password === '') {
+    alert('Please fill in all fields!')
+    return
+  }
+  const collecTionName = 'accounts'
+  const querySnapshot = await getDocs(collection(db, collecTionName))
+  querySnapshot.forEach((doc) => {
+    if(doc.data().username === form.value.username && doc.data().password === form.value.password) {
+      alert('Login successful!')
+      router.push('/')
+    }
+    else {
+      alert('Invalid username or password!')
+    }
+  })
+}
 
 </script>
 
