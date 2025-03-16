77068fe64ebb7b2fb3fd06681fc97801a2299904
Joerick Amadora <124227727+joricxxx@users.noreply.github.com>
2025-03-16 11:51:43 +0800

    Add Logout Dialogue

--- components/EmployeeModal.vue
+++ components/EmployeeModal.vue
@@ -19,87 +19,115 @@
   </template>
   
 <script lang="ts" setup>
+import { ref, computed, watch } from "vue";
 import { initializeApp } from "firebase/app";
 import { doc, getFirestore, setDoc, updateDoc } from "firebase/firestore";
 import type { Account } from "~/types/account";
 import type { Employee } from "~/types/employee";
 
-const props = defineProps<{employee?: Employee}>()
+const props = defineProps<{ employee?: Employee }>();
 
-const isEditing = computed(() => !!props.employee)
+const isEditing = computed(() => !!props.employee);
 
-const title = computed(() => isEditing.value ? 'Edit Employee' : 'Add Employee')
-const description = computed(() => isEditing.value ? 'Edit the employee details' : 'Add a new employee')
-const buttonText = computed(() => isEditing.value ? 'Update Employee' : 'Add Employee')
-const open = defineModel({default: false})
+// Modal Info
+const title = computed(() => (isEditing.value ? "Edit Employee" : "Add Employee"));
+const description = computed(() => (isEditing.value ? "Edit the employee details" : "Add a new employee"));
+const buttonText = computed(() => (isEditing.value ? "Update Employee" : "Add Employee"));
+
+// Modal State
+const open = defineModel({ default: false });
 
 // Setup Firebase
 const firebaseConfig = useRuntimeConfig().public;
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 
-const account = useState<Account>('account')
-const currentUser = computed(() => account.value || null)
-
-const currenUserID = computed(() => currentUser.value?.uid)
-const userID = currenUserID.value || "defaultUser"
+// Get User
+const account = useState<Account>("account");
+const currentUser = computed(() => account.value || null);
+const currenUserID = computed(() => currentUser.value?.uid);
+const userID = currenUserID.value || "defaultUser";
+// File Path
 const filepath = `collections/${userID}/employees`;
 
 const emit = defineEmits<{
-    (e: 'employeeUpdated'): void
-}>()
+  (e: "employeeUpdated"): void;
+}>();
 
 const form = ref({
-    name: '',
-    title: '',
-    email: '',
-    sex: ''
-})
-watch(
-    () => props.employee,
-    (newEmployee) => {
-        if(newEmployee) {
-            form.value = {
-              name: newEmployee.name,
-              title: newEmployee.title,
-              email: newEmployee.email,
-              sex: newEmployee.sex
-            }
-        }
-    },
-    {immediate: true}
-)
-const handleSubmit = async () => {
-    try {
-        if(isEditing.value) {
-            const employeeRef = doc(db, filepath, props.employee?.id!)
-            await updateDoc(employeeRef, form.value)
-            useToast().toast({
-                description: 'Update successfully!',
-                variant: 'success',
-                icon: 'lucide:badge-check'
-            })
-        } else {
-            const genAlhaNumericId = Math.random().toString(36).substring(2) 
-            await setDoc(doc(db, filepath, genAlhaNumericId), form.value)
-            useToast().toast({
-                description: 'Employee added successfully!',
-                variant: 'success',
-                icon: 'lucide:badge-check'
-            })
-        }
-        emit('employeeUpdated')
-        open.value = false
-    }
-    catch (error) {
-        useToast().toast({
-            description: 'An error occurred while adding the employee',
-            variant: 'destructive',
-            icon: 'lucide:badge-x'
-        })
-    }
-}
+  name: "",
+  title: "",
+  email: "",
+  sex: ""
+});
 
+// Watch for changes in employee prop to handle form state
+watch(
+  () => props.employee,
+  (newEmployee) => {
+    if (newEmployee) {
+      form.value = {
+        name: newEmployee.name,
+        title: newEmployee.title,
+        email: newEmployee.email,
+        sex: newEmployee.sex
+      };
+    } else {
+      form.value = {
+        name: "",
+        title: "",
+        email: "",
+        sex: ""
+      };
+    }
+  },
+  { immediate: true }
+);
+
+// Also clear the form when the dialog is opened and there is no employee
+watch(
+  () => open.value,
+  (newValue) => {
+    if (newValue && !props.employee) {
+      form.value = {
+        name: "",
+        title: "",
+        email: "",
+        sex: ""
+      };
+    }
+  }
+);
+
+const handleSubmit = async () => {
+  try {
+    if (isEditing.value) {
+      const employeeRef = doc(db, filepath, props.employee?.id!);
+      await updateDoc(employeeRef, form.value);
+      useToast().toast({
+        description: "Update successfully!",
+        variant: "success",
+        icon: "lucide:badge-check"
+      });
+    } else {
+      const genAlphaNumericId = Math.random().toString(36).substring(2);
+      await setDoc(doc(db, filepath, genAlphaNumericId), form.value);
+      useToast().toast({
+        description: "Employee added successfully!",
+        variant: "success",
+        icon: "lucide:badge-check"
+      });
+    }
+    emit("employeeUpdated");
+    open.value = false;
+  } catch (error) {
+    useToast().toast({
+      description: "An error occurred while adding the employee",
+      variant: "destructive",
+      icon: "lucide:badge-x"
+    });
+  }
+};
 </script>
 
 <style>
---
+++ components/LogoutDialogue/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="flex justify-center">
+      <UiAlertDialog>
+        <UiAlertDialogTrigger as-child>
+          <UiButton variant="destructive">Logout</UiButton>
+        </UiAlertDialogTrigger>
+        <UiAlertDialogContent>
+          <div class="flex flex-col gap-2 max-sm:items-center sm:flex-row sm:gap-4">
+            <div
+              class="flex size-9 shrink-0 items-center justify-center rounded-full border border-border"
+              aria-hidden="true"
+            >
+              <Icon name="lucide:circle-alert" class="size-4 opacity-80" />
+            </div>
+            <UiAlertDialogHeader>
+              <UiAlertDialogTitle>Are you sure?</UiAlertDialogTitle>
+              <UiAlertDialogDescription>
+                Are you sure you want to logout?
+              </UiAlertDialogDescription>
+            </UiAlertDialogHeader>
+          </div>
+          <UiAlertDialogFooter>
+            <UiAlertDialogCancel>Cancel</UiAlertDialogCancel>
+            <UiAlertDialogAction @click="handleLogout" >Confirm</UiAlertDialogAction>
+          </UiAlertDialogFooter>
+        </UiAlertDialogContent>
+      </UiAlertDialog>
+    </div>
+  </template>
+<script lang="ts" setup>
+const router = useRouter()
+const userState = useState('account')
+const userCookie = useCookie('account')
+
+const handleLogout = () => {
+  userState.value = null;
+  userCookie.value = null;
+  router.push('/login');
+
+}
+</script>
+  
